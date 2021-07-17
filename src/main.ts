0dfbf1cef449e2ad4b826ca705e52227e529b1c5
litsynp <nocte55is@gmail.com>
2021-07-17 12:08:00 +0900

    feat: add global API prefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* Set global API prefix */
   app.setGlobalPrefix('v1');
+
   app.enableCors();
   await app.listen(3000);
 }
