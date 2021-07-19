b8be10271f3320a75d3ebe54c000aa5504b6fc9f
litsynp <nocte55is@gmail.com>
2021-07-19 14:58:35 +0900

    refactor: remove unused import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
-import { StoresModule } from '../stores/stores.module';
 import { Store } from '../stores/store.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Store]), StoresModule],
+  imports: [TypeOrmModule.forFeature([User, Store])],
   exports: [UsersService],
   providers: [UsersService],
   controllers: [UsersController],
