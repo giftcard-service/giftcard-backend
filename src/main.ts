9e360df0cde0403045e57c078a1856ddb072f439
litsynp <nocte55is@gmail.com>
2021-07-27 17:17:28 +0900

    fix: change port

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, swaggerCustomOptions);
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
